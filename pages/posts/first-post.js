9e0bc1f631d1aaf5bbfe6ce424ea1393d9c43717
haris0 <hari.surrisyad@gmail.com>
2021-03-16 18:56:21 +0700

    handle assets and meta data

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 const FirstPost = () => {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
           <a>Back To Home</a>
         </Link>
       </h2>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   )
 }

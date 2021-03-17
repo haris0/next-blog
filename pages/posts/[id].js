1b5cf18a19f2c3943c1321e58436a4aa20df227b
haris0 <hari.surrisyad@gmail.com>
2021-03-17 10:55:46 +0700

    styling post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,9 @@
 import Layout from '../../components/layout'
 import React from 'react'
 import {getAllPostIds, getPostData} from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.scss'
 
 export const getStaticPaths = async() =>{
   const paths = getAllPostIds()
@@ -11,7 +14,7 @@ export const getStaticPaths = async() =>{
 }
 
 export const getStaticProps = async({params}) =>{
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -22,11 +25,20 @@ export const getStaticProps = async({params}) =>{
 const Post = ({postData}) => {
   return (
     <Layout>
-      {postData.title}
-      <br/>
-      {postData.id}
-      <br/>
-      {postData.date}
+      <Head>
+        <title>
+          {postData.title}
+        </title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>
+          {postData.title}
+        </h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}/>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }

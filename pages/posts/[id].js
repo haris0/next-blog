818a56d74fc7eef697deec917d7571be6d7d40fc
haris0 <hari.surrisyad@gmail.com>
2021-03-17 10:38:00 +0700

    implement dynamic routing

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import Layout from '../../components/layout'
+import React from 'react'
+import {getAllPostIds, getPostData} from '../../lib/posts'
+
+export const getStaticPaths = async() =>{
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback:false
+  }
+}
+
+export const getStaticProps = async({params}) =>{
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+const Post = ({postData}) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br/>
+      {postData.id}
+      <br/>
+      {postData.date}
+    </Layout>
+  )
+}
+
+export default Post

e7f47d97920c35ee0e93ba972701d944b289a2d7
haris0 <hari.surrisyad@gmail.com>
2021-03-17 09:16:39 +0700

    styling next js, css component and global css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -21,7 +22,7 @@ const FirstPost = () => {
         width={144} // Desired size with correct aspect ratio
         alt="Your Name"
       />
-    </>
+    </Layout>
   )
 }
 
